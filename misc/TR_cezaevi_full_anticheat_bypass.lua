c7de29f239d4d35316524a70be7e5ed3f5b62c2b
nfpw <153939922+nfpw@users.noreply.github.com>
2026-05-19 20:10:13 +0300

    Update TR_cezaevi_full_anticheat_bypass.lua

--- misc/TR_cezaevi_full_anticheat_bypass.lua
+++ misc/TR_cezaevi_full_anticheat_bypass.lua
@@ -1,57 +1,52 @@
-local player = game:GetService("Players").LocalPlayer
-
-local functions = filtergc("function", function(obj)
+-- made by @nfpw
+for _, obj in pairs(filtergc("function", { IgnoreExecutor = false }, false)) do
     if islclosure(obj) then
-        local info = debug.getinfo(obj, "S")
-        return info and info.source and (info.source:find("Cloudash.Client") or info.source:find("haha") or info.source:find("LocalScript"))
-    end
-    return false
-end)
-
-for _, obj in ipairs(functions) do
-    local success, upvalues = pcall(debug.getupvalues, obj)
-    if success and upvalues then
-        for i, upval in ipairs(upvalues) do
-            if type(upval) == "table" then
-                for _, v in pairs(upval) do
-                    if type(v) == "string" and (v:find("AppUpdateService") or v:find("UGCValidationService") or v:find("VoiceChat2") or v:find("VoiceChat3")) then
-                        local upv = debug.getupvalues(obj)
-                        for idx, val in ipairs(upv) do
-                            if type(val) == "function" then
-                                hookfunction(val, function() warn("blocked crash/voicechat") end)
-                            elseif type(val) == "table" then
-                                if rawget(val, "FireServer") then
-                                    hookfunction(val.FireServer, function(...) warn("blocked remote") end)
+        local info = debug.getinfo(obj)
+        if info and info.source and (info.source:find("Cloudash.Client") or info.source:find("haha") or info.source:find("LocalScript")) then
+            local success, upvalues = pcall(debug.getupvalues, obj)
+            if success and upvalues then
+                for i, upval in ipairs(upvalues) do
+                    if type(upval) == "table" then
+                        for _, v in pairs(upval) do
+                            if type(v) == "string" and (v:find("AppUpdateService") or v:find("UGCValidationService") or v:find("VoiceChat2") or v:find("VoiceChat3")) then
+                                for idx, val in ipairs(debug.getupvalues(obj)) do
+                                    if type(val) == "function" then
+                                        hookfunction(val, function() return true end)
+                                        print("[cezaevi anticheat] crash hooked ".. idx)
+                                    elseif type(val) == "table" then
+                                        if rawget(val, "FireServer") then hookfunction(val.FireServer, function(...) return true end) end
+                                        if rawget(val, "InvokeServer") then hookfunction(val.InvokeServer, function(...) return true end) end
+                                        print("[cezaevi anticheat] events hooked [1]".. idx)
+                                    end
                                 end
-                                if rawget(val, "InvokeServer") then
-                                    hookfunction(val.InvokeServer, function(...) warn("blocked remote") end)
-                                end
+                                if rawget(upval, "FireServer") then hookfunction(upval.FireServer, function(...) return true end) end
+                                if rawget(upval, "InvokeServer") then hookfunction(upval.InvokeServer, function(...) return true end) end
+                                print("[cezaevi anticheat] events hooked [2] ".. v)
                             end
                         end
                     end
                 end
-            elseif type(upval) == "table" then
-                if rawget(upval, "FireServer") then
-                    hookfunction(upval.FireServer, function(...) warn("blocked remote") end)
-                end
-                if rawget(upval, "InvokeServer") then
-                    hookfunction(upval.InvokeServer, function(...) warn("blocked remote") end)
-                end
             end
+        end
+    end
+end; print("[cezaevi anticheat] hooked")
+
+local lp = cloneref(game.GetService(game, "Players")).LocalPlayer; if lp and lp.Character then
+    local hrp = lp.Character.FindFirstChild(lp.Character, "HumanoidRootPart") or lp.Character.WaitForChild(lp.Character, "HumanoidRootPart", 5)
+    if hrp then
+        for _, connection in pairs(getconnections(hrp.ChildAdded)) do
+            connection:Disconnect()
+            print("fly check hooked [2]")
         end
     end
 end
 
-if player and player.Character then
-    local hrp = player.Character:WaitForChild("HumanoidRootPart")
-    for _, connection in pairs(getconnections(hrp.ChildAdded)) do
-        connection:Disconnect()
-    end
-end
-
-player.CharacterAdded:Connect(function(char)
-    local hrp = char:WaitForChild("HumanoidRootPart")
-    for _, connection in pairs(getconnections(hrp.ChildAdded)) do
-        connection:Disconnect()
+lp.CharacterAdded:Connect(function(char)
+    local hrp = char.FindFirstChild(char, "HumanoidRootPart") or char.WaitForChild(char, "HumanoidRootPart", 5)
+    if hrp then
+        for _, connection in pairs(getconnections(hrp.ChildAdded)) do
+            connection:Disconnect()
+            print("fly check hooked [1]")
+        end
     end
 end)
